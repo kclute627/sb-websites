ad997daf56abed2e76bb548551a9d7be5b3283ff
Kyle C <kyclutter@gmail.com>
2019-09-26 08:16:32 -0500

    updates

--- src/Components/Pricing/Pricing.js
+++ src/Components/Pricing/Pricing.js
@@ -16,9 +16,21 @@ class Pricing extends Component {
             description2={"Search Engine Optomizied"}
             description3={"As many sections as you want"}
           
-                      />
-          <PricingBox />
-          <PricingBox />
+                      /> 
+          <PricingBox 
+          header={"5 Page Website"}
+          pricing={"$999"}
+          description1={"5 Page Website"}
+          description2={"Search Engine Optomizied"}
+          description3={"Home, About, Contact + 2 other pages"}
+          popular = {true}/>
+          <PricingBox
+           header={"10 Page Website"}
+           pricing={"$1699"}
+           description1={"10 Page Website + 1 year free hosting"}
+           description2={"Search Engine Optomizied"}
+           description3={"Home, About, Contact + 7 other pages"}
+             />
           
         </div>
       </div>
--- src/Components/Pricing/PricingBox.js
+++ src/Components/Pricing/PricingBox.js
@@ -1,37 +1,44 @@
-import React from 'react';
-import ReactSvg from 'react-svg';
-import  check from '../../assets/tick.svg';
-
-
-
+import React from "react";
+import ReactSvg from "react-svg";
+import check from "../../assets/tick.svg";
 
 const PricingBox = props => {
-
-
-    return(
-        <div className="pricing__pricingBox">
-            <div className="pricing__pricingBox-header">
-                {props.header}
-            </div>
-            <div className="pricing__pricingBox-price">
-                {props.pricing}
-            </div>
-            <div className="pricing__pricingBox-description">
-                <ul>
-                    <li className="pricing__pricingBox-description-4"> <ReactSvg src={check}/></li>
-                    <li className="pricing__pricingBox-description-1">{props.description1}</li>
-                    <li className="pricing__pricingBox-description-2">{props.description2}</li>
-                    <li className="pricing__pricingBox-description-3">{props.description3}</li>
-                </ul>
-            </div>
-            <button className="pricing__pricingBox-cta">
-                Buy Now
-            </button>
-            
-        </div>
-    )
-}
-
-
+  return (
+     
+    <div className="pricing__pricingBox">
+       <div className={props.popular ? "pricing__pricingBox-popular":  "pricing__pricingBox-popular-1" }>Most Popular</div>
+      <div className="pricing__pricingBox-header">{props.header}</div>
+      <div className="pricing__pricingBox-price">{props.pricing}</div>
+      <div className="pricing__pricingBox-description">
+        <ul>
+          <li className="pricing__pricingBox-description-4">
+            {" "}
+            <ReactSvg src={check} />
+          </li>
+          <li className="pricing__pricingBox-description-1">
+            {props.description1}
+          </li>
+          <li className="pricing__pricingBox-description-5">
+            {" "}
+            <ReactSvg src={check} />
+          </li>
+          <li className="pricing__pricingBox-description-2">
+            {props.description2}
+          </li>
+          <li className="pricing__pricingBox-description-6">
+            {" "}
+            <ReactSvg src={check} />
+          </li>
+          <li className="pricing__pricingBox-description-3">
+            {props.description3}
+          </li>
+         
+          <a className={props.popular ? "pricing__pricingBox-cta popular" : "pricing__pricingBox-cta" }>Get Started</a>
+        </ul>
+      </div>
+    </div>
+    
+  );
+};
 
 export default PricingBox;
